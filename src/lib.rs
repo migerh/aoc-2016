fe04f32551337fa7c563dc8a5b4bfc7497f1aff9
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2022-01-03 11:54:59 +0100

    Bootstrap AoC 2016

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+mod utils;
+
+aoc_lib!{ year = 2016 }
---
+++ src/template/mod.rs
@@ -0,0 +1,26 @@
+use crate::utils::ParseError;
+
+#[aoc_generator(dayXX)]
+fn get_input(input: &str) -> Result<isize, ParseError> {
+    Ok(0)
+}
+
+#[aoc(dayXX, part1)]
+fn problem1(input: &isize) -> Result<usize, ParseError> {
+    Ok(0)
+}
+
+#[aoc(dayXX, part2)]
+fn problem2(input: &isize) -> Result<usize, ParseError> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn example_1_1() {
+        assert_eq!(0, 0);
+    }
+}
